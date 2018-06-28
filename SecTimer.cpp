e7873798b03e67a9a1f5ff8121957de90d74985f
rebe0909 <40675071+rebe0909@users.noreply.github.com>
2018-06-28 23:04:18 +0200

    intital Files added

---
+++ SecTimer.cpp
@@ -0,0 +1,47 @@
+/*
+	SecTimer.cpp
+*/
+#include "Arduino.h"
+#include "SecTimer.h"
+
+
+SecTimer::SecTimer()
+{
+	_timer.starttime = 0;
+	_timer.stoptime = 0;
+	_timer.running = false;
+}
+
+bool SecTimer::hasElapsedTimer (void)
+{
+	return _hasBeenStartedSekundentimer () && _msTimerGetTimeRemaining() == 0;
+
+}
+
+SecTimer::startTimer(int Sec)
+{
+	_timer.starttime = millis();
+	_timer.stoptime = _timer.starttime + (Sec * 1000);
+	_timer.interval = Sec * 1000;
+	_timer.running = true;
+}
+
+bool SecTimer::_hasBeenStartedSekundentimer (void)
+{
+	return _timer.starttime != _timer.stoptime || _timer.running;
+}
+
+unsigned long SecTimer::_msTimerGetTimeRemaining(void)
+{
+	unsigned long _current = millis();
+	if((_current - _timer.starttime) > _timer.interval) 
+	{
+		_timer.running = false;
+		_timer.interval = 0;
+		return 0;
+	}
+	else
+	{
+		return _current - _timer.stoptime;
+	}
+}
---
+++ SecTimer.h
@@ -0,0 +1,26 @@
+#ifndef __SEC_TIMER__
+#define __SEC_TIMER__
+#include "Arduino.h"
+
+
+class SecTimer {
+	
+public:
+		bool trueforSeconds (bool bedingung, INT sek);
+		bool hasElapsedTimer (void);
+		bool hasElapsedOrNotBeenStartedSec (void);
+		void startTimer(int Sec);
+		typedef struct 
+		{
+			unsigned long starttime_ms;
+			unsigned long stoptime_ms;
+			unsigned long interval;
+			bool running;
+		} ms_timer_t;
+private: 
+		ms_timer_t _timer;
+		bool hasBeenStartedSecTimer(void);
+
+}
+
+#endif
