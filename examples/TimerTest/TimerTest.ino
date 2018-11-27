dcd29ba136207e9ab0b7ae2daa0468d39c2fcad7
rebe0909 <40675071+rebe0909@users.noreply.github.com>
2018-11-27 07:56:00 +0100

    Update TimerTest.ino

--- examples/TimerTest/TimerTest.ino
+++ examples/TimerTest/TimerTest.ino
@@ -1,11 +1,11 @@
 #include arduino.h
 #include <SecTimer.h>
 
-SecTimer timer[8];
+SecTimer timer[8]; //Declare 8 Timers
 
 void setup() {
   // put your setup code here, to run once:
-  timer[0].startTimer(15);
+  timer[0].startTimer(15); //start one Timer
 }
 
 void loop() {
@@ -14,5 +14,6 @@ bool timerElapsed = timer[0].hasElapsedTimer();
   if(timerElapsed)
   {
     //Do_something....
+    timer[0].startTimer(10); //Start Timer again....
   }
 }
