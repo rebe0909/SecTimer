9e659111d496063aba4ded3d930a2a84f01aa5eb
Reinhard Beraus <reinhard.beraus@kwb.at>
2018-11-27 14:26:27 +0100

    only code style

--- examples/TimerTest/TimerTest.ino
+++ examples/TimerTest/TimerTest.ino
@@ -3,13 +3,16 @@
 
 SecTimer timer[8]; //Declare 8 Timers
 
-void setup() {
-  // put your setup code here, to run once:
-  timer[0].startTimer(15); //start one Timer
+void setup() 
+{
+
+	// put your setup code here, to run once:
+	timer[0].startTimer(15); //start one Timer
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
+	// put your main code here, to run repeatedly:
 	bool timerElapsed = timer[0].hasElapsedTimer();
 	if(timerElapsed)
 	{
